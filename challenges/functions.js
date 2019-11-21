939eee638f6ad07387df3f5b21fa2e4f600ec64f
Adam <adammonast@Adams-MBP.attlocal.net>
2019-11-21 15:55:31 -0500

    revised JS sprint challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,19 +17,30 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return cb(num1 + num2)
+}
+
+function multiply(num1, num2){
+  return cb(num1 * num2)
+}
+
+function greeting(firstName, lastName){
+  returns `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
-
+// Explanation: Closure enables this to happen. A nested function can be returned as either a property of a new object (if the outer function creates an object with methods) or as a result by itself. It can then be used somewhere else. No matter where, it still has access to the same outer variables.
 
 const external = "I'm outside the function";
 
